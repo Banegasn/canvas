0551e4024eb49f866904982c533a854907b142be
banegasn <nicobanegas.sc@gmail.com>
2024-09-16 09:35:08 +0200

    nada

--- server.mjs
+++ server.mjs
@@ -5,28 +5,44 @@ const wss = new WebSocketServer({ port: 8080 });
 console.log('WebSocket server is running on ws://localhost:8080');
 
 /**
+ * @typedef {{x: number, y: number, color: string }} PaintMessage
+ */
+
+/**
  * @type {WebSocket[]}
  */
 let clients = [];
-let quantityOfLacasitos = 0
+
+/**
+ * @type {number}
+ */
+let pixels = 0
+
+/**
+ * When a message is received, broadcast to all clients
+ * @param {PaintMessage} message 
+ */
+const newMessage = (message) => {
+    pixels++;
+    clients.forEach(client => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message, { binary: false });
+        }
+    });
+};
+
+/**
+ * Clean up when clients disconnect
+ */
+const closeConnection = () => {
+    clients = clients.filter(client => client !== ws);
+};
+
 
 wss.on('connection', (ws) => {
     clients.push(ws);
 
-    // When a message is received, broadcast to all clients
-    ws.on('message', (message) => {
-        quantityOfLacasitos++;
-        clients.forEach(client => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message, { binary: false });
-            }
-        });
-        console.log(quantityOfLacasitos);
-    });
+    ws.on('message', newMessage);
 
-    // Clean up when clients disconnect
-    ws.on('close', () => {
-        clients = clients.filter(client => client !== ws);
-    });
-
+    ws.on('close', closeConnection);
 });
